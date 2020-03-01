0e8e8d73ddd917ca7e30ba88ab6f3a3077af8769
Manuel Zaforas <mzaforas@gmail.com>
2020-03-01 16:39:27 +0100

    Mapping between attractions names and ids

--- disney_api/get_park_info.py
+++ disney_api/get_park_info.py
@@ -1,10 +1,31 @@
 import requests
 import uuid
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 
 ENDPOINT = 'https://api.disneylandparis.com/query'
-HEADERS = {
-    'x-application-id': 'mobile-app',
+
+
+_transport = RequestsHTTPTransport(
+    url=ENDPOINT,
+    use_json=True,
+)
+
+
+client = Client(
+    transport=_transport,
+    fetch_schema_from_transport=True,
+)
+query = gql("""
+{
+    activities(market: "es-es", types: ["Attraction"]) {
+        name, id
+    }
 }
+""")
+
+print(client.execute(query))
+
 #        query: 'query
 
 # activities($market: String!, $types: [String])
@@ -12,13 +33,3 @@ HEADERS = {
 #   { contentType: __typename entityType contentId id name location { ...location } coordinates { ...coordinates } closed schedules { language startTime endTime date status closed } ... on Attraction { age { ...facet } height { ...facet } interests { ...facet } photopass fastPass singleRider } } }
 # fragment facet on Facet { id value urlFriendlyId iconFont } fragment location on Location { id value urlFriendlyId iconFont } fragment coordinates on MapCoordinates { lat lng type } ',
 #query = {'query': 'query activities(en-gb: String!, ["Attraction"]: [String]) { activities(market: en-gb, types: ["Attraction"]) { contentType: __typename entityType contentId id name location { ...location } coordinates { ...coordinates } closed schedules { language startTime endTime date status closed } ... on Attraction { age { ...facet } height { ...facet } interests { ...facet } photopass fastPass singleRider } } } fragment facet on Facet { id value urlFriendlyId iconFont } fragment location on Location { id value urlFriendlyId iconFont } fragment coordinates on MapCoordinates { lat lng type } '}
-data = {
-    'query': 'query activities($market: String!, $types: [String]) { activities(market: $market, types: $types) { contentType: __typename entityType contentId id name location { ...location } coordinates { ...coordinates } closed schedules { language startTime endTime date status closed } ... on Attraction { age { ...facet } height { ...facet } interests { ...facet } photopass fastPass singleRider } } } fragment facet on Facet { id value urlFriendlyId iconFont } fragment location on Location { id value urlFriendlyId iconFont } fragment coordinates on MapCoordinates { lat lng type } ',
-    'variables': {
-        'market': 'en-gb',
-        'types': ['Attraction'],
-    }
-}
-
-r = requests.post(ENDPOINT, headers=HEADERS, data=data)
-print(r.status_code, r.text)
